b4bfbb18a9e063cd33398083a082dfc8efc46acd
Mohammed Rabil <rabil@techie.com>
2023-04-25 13:01:53 +0530

    Change to card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import FeatureCard from '@/components/FeatureCard'
+import Datacleaning from '@/icons/Datacleaning'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,22 +16,22 @@ export default function Home() {
         <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
       </div>
       <div className='grid place-items-center grid-cols-3 gap-4 p-4'>
-        <div className='p-4 h-36 w-full max-w-36 border-2 border-secondary rounded-lg flex flex-col items-center justify-center bg-blue-600'>
-          <h1>
-            Data Cleaning
-          </h1>
-          <p></p>
-        </div>
-        <div className='p-4 h-36 w-full max-w-36 border-2 border-secondary rounded-lg flex flex-col items-center justify-center bg-blue-600'>
-          <h1>
-            Data Visualization
-          </h1>
-        </div>
-        <div className='p-4 h-36 w-full max-w-36 border-2 border-secondary rounded-lg flex flex-col items-center justify-center bg-blue-600'>
-          <h1>
-            Data Analysation
-          </h1>
-        </div>
+        <FeatureCard title="Data Cleaning" description="Clean your data with our data cleaning tool" icon={
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+          </svg>
+
+        } button="Clean Data" />
+        <FeatureCard title="Data Visualization" description="Visualize your data with our data visualization tool" icon={
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+          </svg>
+        } button="Visualize Data" />
+        <FeatureCard title="Data Analysis" description="Analyze your data with our data analysis tool" icon={
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+          </svg>
+        } button="Analyze Data" />
       </div>
     </main>
   )
