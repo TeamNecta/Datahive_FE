a004e37af5a636ce6f7998bb7454b411711dd63e
Mohammed Rabil <rabil@techie.com>
2023-04-25 13:01:00 +0530

    init feature card component

---
+++ components/FeatureCard.tsx
@@ -0,0 +1,27 @@
+import React, { JSXElementConstructor, ReactElement } from 'react'
+
+type FeatureCardProp = {
+    title: string,
+    description: string,
+    icon: ReactElement,
+    button: string
+}
+
+const FeatureCard = (props: FeatureCardProp) => {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="p-2">
+                {props.icon}
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{props.title}</h2>
+                <p>{props.description}</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">{props.button}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeatureCard
