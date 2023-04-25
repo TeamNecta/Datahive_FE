7940e4ca095e4b8cb516533af470b20273430fff
Mohammed Rabil <rabil@techie.com>
2023-04-25 21:29:13 +0530

    Add api redirection

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://datahivebe.rabil.me/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
