601118827ec3b70c4042063b6b41fae696ca205b
Mohammed Rabil <rabil@techie.com>
2023-04-25 12:08:54 +0530

    Init about page

---
+++ pages/about.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <main className='container mx-auto min-h-screen bg-gradient-to-r from-purple-600 to-blue-600 flex flex-col h-screen items-center justify-center'>
+            <h1 className='text-4xl font-bold'>About</h1>
+            <div className="max-w-2xl mt-4">
+                <p>
+                    In today's digital age, data analysis and visualization have become crucial aspects of decision-making, both in the business world and in everyday life. However, accessing the right tools to perform these tasks can be a challenge, especially for those who lack the expertise and resources to do so. That is where our new data analysis and visualization website comes in.
+                </p>
+                <br />
+                <p>
+                    Our website is designed to be accessible and user-friendly, with a focus on empowering the general public to analyse and visualize their data. It accepts datasets in any format and generate insights from the data. Additionally, it can train predictive models on the data using scikit-learn, allowing users to make accurate predictions about future trends.
+                </p>
+                <br />
+                The website's interface is simple and user-friendly, allowing users to easily upload their data and navigate through the analysis and visualization tools. Users can choose from a variety of visualization options, including graphs, charts, and maps, to help them better understand their data. Additionally, the website offers customization options, allowing users to tailor the visualizations to their specific needs.
+                <p>
+                    <br />
+                    One of the key features of our website is its ability to generate insights and recommendations based on the data. The website's algorithms analyse the data to identify patterns and trends, providing users with valuable information that they can use to make informed decisions.
+                </p>
+                <br />
+                <p>
+                    Overall, our data analysis and visualization website will be an invaluable tool for anyone looking to gain insights from their data. By making these tools accessible to the general public, we hope to empower individuals and organizations to make better decisions and drive positive change.
+                </p>
+            </div>
+        </main>
+    )
+};
+
+export default about
