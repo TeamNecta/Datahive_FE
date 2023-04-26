d85f4ef169b683ab42eead667cbe3a991be57ebd
Mohammed Rabil <rabil@techie.com>
2023-04-26 06:43:30 +0530

    Refactor home icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import FeatureCard from '@/components/FeatureCard'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import Image from 'next/image' 
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -45,20 +46,14 @@ export default function Home() {
       </div>
       <div className='grid place-items-center grid-cols-3 gap-4 p-4'>
         <FeatureCard title="Data Cleaning" description="Clean your data with our data cleaning tool" icon={
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-          </svg>
+          <Image src="/datacleaning.svg" alt="Data Cleaning" height={250} width={250} />
 
         } button="Clean Data" />
         <FeatureCard title="Data Visualization" description="Visualize your data with our data visualization tool" icon={
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-          </svg>
+          <Image src="/datavisualization.svg" alt="Data Visualization" height={250} width={250} />
         } button="Visualize Data" />
         <FeatureCard title="Data Analysis" description="Analyze your data with our data analysis tool" icon={
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-          </svg>
+          <Image src="/dataanalysis.svg" alt="Data Analysis" height={250} width={250} />
         } button="Analyze Data" />
       </div>
     </main>
